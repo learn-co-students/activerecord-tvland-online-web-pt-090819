62fb05bdc273b3fc2eca2b07fe73a54245a78288
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-11-27 03:37:30 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
-  has_many :actors, through, :characters
+  has_many :actors, through: :characters
   
   def actor_list
     self.actor.map {|a| a.full_name}
